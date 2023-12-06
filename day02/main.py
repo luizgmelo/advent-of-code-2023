8a721c663178c83d76a78b61bf3b507426b0def4
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2023-12-05 23:44:35 -0300

    Day 02 part 2

--- day02/main.py
+++ day02/main.py
@@ -1,21 +1,27 @@
 with open('puzzle_input.txt', 'r') as f:
     s = f.read()
 
-# with open('notsolvedyet.txt', 'r') as f:
-#     s = f.read()
-
 game_number = 1
 possible = True
 sum_ids = 0
+sum_sets = 0
 for line in s.strip().split("\n"):
     possible = True
     line = line.split(":")
     game = line[0]
+    power = 1
+    maxColors = {
+            "red": 0,
+            "green": 0,
+            "blue": 0
+            }
     for random in line[1].split(";"):
         for cubes in random.split(","):
             cubes = cubes.strip().split(" ")
             amount = int(cubes[0])
             color = cubes[1]
+            if maxColors[color] < amount:
+                maxColors[color] = amount
             if amount > 12 and color == 'red':
                 possible = False
             if amount > 13 and color == 'green':
@@ -23,9 +29,16 @@ for line in s.strip().split("\n"):
             if amount > 14 and color == 'blue':
                 possible = False
 
+    if maxColors:
+        power *= maxColors["red"] * maxColors["blue"] * maxColors["green"]
+        sum_sets += power
+
     if possible == True:
         sum_ids += game_number
+
+    
     game_number += 1
 
-print(sum_ids) 
 
+print("Sum ids:", sum_ids)
+print("Sum sets:", sum_sets)
